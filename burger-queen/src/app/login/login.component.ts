f9f2021fbb88d65f0ea58747ac44ac5ec9e92a64
mmayalae <mmayalae@pichincha.com>
2023-06-21 00:53:37 -0500

    se agrega ultimas validaciones en login

--- burger-queen/src/app/login/login.component.ts
+++ burger-queen/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,20 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit  {
-  constructor() {   }
+  constructor(private router: Router) {   }
+
+  get correo(){
+    return this.loginForm.get('correo') as FormControl;
+  }
+  get clave(){
+    return this.loginForm.get('clave') as FormControl;
+  }
+
+  sendForm(){
+    console.log(this.loginForm.value);
+    const ruta = '../manager';
+    this.router.navigate([ruta]);
+  }
 
   loginForm = new FormGroup ({
     'correo': new FormControl('', [Validators.required, Validators.email]),
