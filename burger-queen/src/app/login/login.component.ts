01ced1f75b254ad61a9ea9671bfee53ccf581d6a
mmayalae <mmayalae@pichincha.com>
2023-06-20 23:51:00 -0500

    se realiza validacion de formulario login

--- burger-queen/src/app/login/login.component.ts
+++ burger-queen/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit  {
   constructor() {   }
 
+  loginForm = new FormGroup ({
+    'correo': new FormControl('', [Validators.required, Validators.email]),
+     'clave': new FormControl('', Validators.required),
+  })  
+ 
+  
+
+  
+
   ngOnInit(): void {
 
   }
+  
+  
 
-  iniciar_sesion(email:string){
-    console.log(email)
-  }
 
   imagenes:string = 'https://i.ibb.co/vZtH272/imgLogo.png'
 }
