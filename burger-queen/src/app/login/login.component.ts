8f904715d92a2d417a00e5eff572702e1a9e64d8
mmayalae <mmayalae@pichincha.com>
2023-06-27 20:20:05 -0500

    prueba y error de codigo para mostrar error en consola

--- burger-queen/src/app/login/login.component.ts
+++ burger-queen/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import {LoginI} from '../interfaces/login.interface';
 
 @Component({
   selector: 'app-login',
@@ -9,33 +10,35 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit  {
+  loginForm = new FormGroup ({
+    'email': new FormControl('', Validators.required),
+     'password': new FormControl('', Validators.required)
+  })  
+
   constructor(private router: Router,private user: AuthService ) {   }
   
 
   get correo(){
-    return this.loginForm.get('correo') as FormControl;
+    return this.loginForm.get('email') as FormControl;
   }
   get clave(){
-    return this.loginForm.get('clave') as FormControl;
+    return this.loginForm.get('password') as FormControl;
   }
 
-  sendForm(){
-    console.log(this.loginForm.value);
-    const ruta = '../manager';
-    this.router.navigate([ruta]);
+  onLogin(form:LoginI){
+    this.user.loginByEmail(form).subscribe(data =>{
+      console.log(data);
+    })
   }
 
-  loginForm = new FormGroup ({
-    'correo': new FormControl('', [Validators.required, Validators.email]),
-     'clave': new FormControl('', Validators.required),
-  })  
+
  
   
 
   
 
   ngOnInit(): void {
-    this.user.getUser().subscribe(()=>{console.log})
+    //this.user.getUser().subscribe(()=>{console.log})
 
   }
   
--- burger-queen/src/app/services/auth.service.ts
+++ burger-queen/src/app/services/auth.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {LoginI} from '../interfaces/login.interface';
+import {ResponseI} from '../interfaces/response.interface';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  url: string = 'http://localhost:8080/';
   
-  constructor(private http : HttpClient ) {
-    
-   }
-    getUser() {
-      return this.http.post('http://localhost:8080/login',{
-        "email": "anita.borg@systers.xyz",
-        "password": "123456"
-      } )
+  constructor(private http : HttpClient ) { }
+
+    loginByEmail(form:LoginI): Observable<ResponseI> {
+      let direccion = this.url + 'auth';
+      return this.http.post<ResponseI>(direccion, form);
+      
     }
 }
