8c110de1e176b16fd2a064e1243da0b888fc16e9
mmayalae <mmayalae@pichincha.com>
2023-07-03 14:05:25 -0500

    se agrega codigo en servicio products

--- burger-queen/src/app/services/products.service.ts
+++ burger-queen/src/app/services/products.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ProductsI } from '../interfaces/products.interface';
+import { Observable } from 'rxjs';
+import { Token } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  
+  //responseData:ProductsI[]
+  tokenAccess: string | null;
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) {
+    //this.responseData = []; 
+    this.tokenAccess =  localStorage.getItem('token');}
+
+  private apiurl:string = 'http://localhost:8080/products';
+  
+
+ 
+  getDataFromAPI(): Observable<ProductsI[]>{
+    const headers = new HttpHeaders().set('Authorization',`Bearer ${this.tokenAccess}` )
+    return this.http.get<ProductsI[]>(this.apiurl, {headers})
+  }
+  
+  //getProducts(): ProductsI[]{
+   // console.log(this.responseData)
+    //return this.responseData
+  //}
+  //getToken(): string | null {
+    //localStorage.getItem(Token);
+     //this.tokenAccess =  localStorage.getItem('token');
+     //return this.tokenAccess
+
+  }
+  
+
+
+
