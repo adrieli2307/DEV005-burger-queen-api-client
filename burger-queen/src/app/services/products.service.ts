b26b0f9f0ed414c5186d5ebc5ded05d60731b749
adrieli2307 <aearmijosq1104@gmail.com>
2023-07-03 20:25:38 -0500

    Autoformato

--- burger-queen/src/app/services/products.service.ts
+++ burger-queen/src/app/services/products.service.ts
@@ -8,35 +8,35 @@ import { Token } from '@angular/compiler';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductsService {
-  
+
   //responseData:ProductsI[]
   tokenAccess: string | null;
 
   constructor(private http: HttpClient) {
     //this.responseData = []; 
-    this.tokenAccess =  localStorage.getItem('token');}
-
-  private apiurl:string = 'http://localhost:8080/products';
-  
-
- 
-  getDataFromAPI(): Observable<ProductsI[]>{
-    const headers = new HttpHeaders().set('Authorization',`Bearer ${this.tokenAccess}` )
-    return this.http.get<ProductsI[]>(this.apiurl, {headers})
+    this.tokenAccess = localStorage.getItem('token');
   }
-  
+
+  private apiurl: string = 'http://localhost:8080/products';
+
+  getDataFromAPI(): Observable<ProductsI[]> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.tokenAccess}`)
+    return this.http.get<ProductsI[]>(this.apiurl, { headers })
+  }
+
   //getProducts(): ProductsI[]{
-   // console.log(this.responseData)
-    //return this.responseData
+  // console.log(this.responseData)
+  //return this.responseData
   //}
   //getToken(): string | null {
-    //localStorage.getItem(Token);
-     //this.tokenAccess =  localStorage.getItem('token');
-     //return this.tokenAccess
+  //localStorage.getItem(Token);
+  //this.tokenAccess =  localStorage.getItem('token');
+  //return this.tokenAccess
 
-  }
-  
+}
+
 
 
 
